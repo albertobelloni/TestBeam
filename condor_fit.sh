d4259b8e07c858fe63ebd14ef1b504f4ba1f6561
albertobelloni <Alberto.Belloni@cern.ch>
2020-01-27 10:34:09 -0500

    added some code to copy fit output in separate directory; now stdout, stderr and log files get moved to subdirectory, while roofit file is moved by running make clean

--- condor_fit.sh
+++ condor_fit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-################################################################################
+###############################################################################
 #
 # example submission on condor:
 #
@@ -11,14 +11,20 @@
 #    EJ_200, EJ_260, EJ_260_2P, 
 #    SCSN_81F1, SCSN_81F2, SCSN_81F3, SCSN_81F4, SCSN_81S
 #
-# remember to set RUN_DIR and TILE_TO_FIT in the condor_fit.jdl file
+# remember to set TILE_TO_FIT in the condor_fit.jdl file
 #
-################################################################################
+###############################################################################
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
 
 root -b -q run_fitter.C\(\"energy_tree_$1\"\)
+
+mkdir -p condorfits_$2
+
+if [ -e $1.stdout ] && [ -e $1.stderr ] && [ -e $1.log ]; then
+    mv $1.stdout $1.stderr $1.log condorfits_$2/
+fi
 
 exit
 
--- condor_fits.sh
+++ condor_fits.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-################################################################################
+###############################################################################
 #
 # example submission on condor:
 #
 #    condor_submit condor_fits.jdl
 #
-#
-################################################################################
+###############################################################################
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
@@ -16,6 +15,12 @@ tiles=('EJ_260' 'EJ_260_2P' 'EJ_200' 'SCSN_81F1' \
     'SCSN_81F2' 'SCSN_81F3' 'SCSN_81F4' 'SCSN_81S')
 
 root -b -q run_fitter.C\(\"energy_tree_${tiles[$1]}\"\)
+
+mkdir -p condorfits_$2
+
+if [ -e $1.stdout ] && [ -e $1.stderr ] && [ -e $1.log ]; then
+    mv $1.stdout $1.stderr $1.log condorfits_$2/
+fi
 
 exit
 
